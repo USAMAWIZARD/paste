46b0cf7f48fa8df9c3844683721b8430c2753a07
USAMAWIZARD <usamawizard@gmail.com>
2021-03-14 00:02:17 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ express = require("express");
 app = express();
 app.listen(3000);
 app.use(express.static(__dirname));
-const fileUpload = require("express-fileupload");
-app.use(fileUpload());
+//const fileUpload = require("express-fileupload");
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+}); 
+//app.use(fileUpload());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 userdata = {};
+userfiles={};
 
 app.post("/:username/update", (req, res) => {
-
 if (req.body.topaste != "" ) 
   userdata[req.param("username")].push(req.body.topaste);
-  console.log(JSON.parse(JSON.stringify(req.files)));
-  if (req.files != null) 
-    userdata[req.param("username")].push(JSON.stringify(req.files));
+    if (req.file != null){
+    console.log(req.files)
+    upload.single(req.files)
+    userfiles[req.param("username")].push(JSON.stringify(req.file));
+  }
   res.redirect("/" + req.param("username"));
 });
 app.get("/:username/delete", (req, res) => {
@@ -26,8 +32,13 @@ app.get("/:username/delete", (req, res) => {
     res.send("no user exist with username "+req.param("username"))
 });
 app.get("/:username/new", (req, res) => {
+  if(userdata[req.param("username")]==undefined){
   userdata[req.param("username")] = [];
+  userfiles[req.param("username")] = [];
   res.send("new user created " + req.param("username"));
+  }
+  else
+    res.send("user already exist "+req.param("username"))
 });
 app.get("/:username/:pastestring", (req, res) => {
   userdata[req.param("username")].push(req.param("pastestring"));
